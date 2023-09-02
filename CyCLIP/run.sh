a0bcac1b0900291baada3e00cc4b39f5eff12886
Wenhan Yang <bighangeryang@gmail.com>
2023-09-01 19:56:04 -0700

    update on training

--- CyCLIP/run.sh
+++ CyCLIP/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-runNames='1M_baseline'
-lpName='1M_baseline_eval'
-device=1
+runNames='NNCLIP_100K_NN_CROSS'
+lpName='NNCLIP_100K_NN_CROSS_eval'
+device=4
 
-beginEpoch=32
-endEpoch=32
+beginEpoch=26
+endEpoch=31
 batch_size=256
 
 # clean similarity args
@@ -13,29 +13,29 @@ cleanDataPath='../short_100K_clean.csv'
 validationPath='../valid_temp.csv'
 
 # LP args
-eval_data_types=('CIFAR100') # 'caltech101' 'flowers_102' 'food_101')
+eval_data_types=('CIFAR10' 'CIFAR100') # 'caltech101' 'flowers_102' 'food_101')
 # eval_data_types='CIFAR10'
 
 for runName in "${runNames[@]}"
 do 
     for eval_data_type in "${eval_data_types[@]}"
     do
-    mkdir "logs_mine/$runName/LP_output_logs/"
-    mkdir "logs_mine/$runName/ZS_output_logs/"
-    mkdir "logs_mine/$runName/LP_output_logs/$eval_data_type"
-    mkdir "logs_mine/$runName/ZS_output_logs/$eval_data_type"
+    mkdir "logs/$runName/LP_output_logs/"
+    mkdir "logs/$runName/ZS_output_logs/"
+    mkdir "logs/$runName/LP_output_logs/$eval_data_type"
+    mkdir "logs/$runName/ZS_output_logs/$eval_data_type"
     done
 done
 
 # poison eval args
-dataset='imagenet100'
-poison_path='../1M_100_info.csv'
+dataset='imagenet30'
+poison_path='../100K_30_info.csv'
 
 for runName in "${runNames[@]}"
 do
     for ((i=$beginEpoch; i<=$endEpoch; i++))
     do
-        checkpointPath="logs_mine/$runName/checkpoints/epoch_$i.pt"
+        checkpointPath="logs/$runName/checkpoints/epoch_$i.pt"
         
         # get clean similarity
         # python -m src.main --name $runName --train_data $cleanDataPath --validation_data $validationPath --image_key path --caption_key caption --device_id $device --batch_size $batch_size --epoch $i --checkpoint $checkpointPath  --representation 
@@ -49,18 +49,18 @@ do
             # get LP accuracy
             # python -m src.main --name $lpName --eval_data_type $eval_data_type --eval_train_data_dir $eval_train_data_dir --eval_test_data_dir $eval_test_data_dir --device_id $device --checkpoint $checkpointPath --linear_probe --linear_probe_batch_size $batch_size
             # wait
-            # cp "logs/$lpName/output.log" "logs_mine/$runName/LP_output_logs/$eval_data_type/output_epoch$i.log" 
+            # cp "logs/$lpName/output.log" "logs/$runName/LP_output_logs/$eval_data_type/output_epoch$i.log" 
             # wait
 
-            # get ZS accuracy
-            python -m src.main --name $lpName --eval_data_type $eval_data_type  --eval_test_data_dir $eval_test_data_dir --device_id $device --checkpoint $checkpointPath 
-            wait
-            cp "logs/$lpName/output.log" "logs_mine/$runName/ZS_output_logs/$eval_data_type/output_epoch$i.log" 
-            wait
+            # # get ZS accuracy
+            # python -m src.main --name $lpName --eval_data_type $eval_data_type  --eval_test_data_dir $eval_test_data_dir --device_id $device --checkpoint $checkpointPath 
+            # wait
+            # cp "logs/$lpName/output.log" "logs/$runName/ZS_output_logs/$eval_data_type/output_epoch$i.log" 
+            # wait
         done 
 
         # get poison evals 
-        # python verify_with_template_full.py --model_name $runName --device $device --epoch $i --dataset $dataset --path $poison_path --distributed
-        # wait
+        python verify_with_template_full.py --model_name $runName --device $device --epoch $i --dataset $dataset --path $poison_path --distributed
+        wait
     done
 done
--- CyCLIP/src/main.py
+++ CyCLIP/src/main.py
@@ -102,18 +102,17 @@ def worker(rank, options, logger):
         print("Pretrain loader number of samples: ", pretrain_loader.num_samples)
         # train_loader = get_subset_dataloader(options, data['train_set'], range(len(data['train_set'])))
         # all_loader = get_subset_dataloader(options, data['train_set'], range(len(data['train_set'])), drop_last=False)
-        optimizer = optim.AdamW([{"params": no_weight_decay_parameters, "weight_decay": 0}, {"params": weight_decay_parameters, "weight_decay": options.weight_decay}], lr = options.lr, betas = (options.beta1, options.beta2), eps = options.eps)
+        optimizer = optim.AdamW([{"params": no_weight_decay_parameters, "weight_decay": 0}, {"params": weight_decay_parameters, "weight_decay": options.weight_decay}], lr = options.in_lr, betas = (options.beta1, options.beta2), eps = options.eps)
         # scheduler = cosine_scheduler(optimizer, options.lr, options.post_lr, 
         #                 options.num_warmup_steps,
         #                 pretrain_loader.num_batches * (options.inmodal_warmup+options.multimodal_warmup+1),
         #                 pretrain_loader.num_batches * 32) #options.epochs)
         #                 # train_loader.num_batches * (options.inmodal_warmup+options.multimodal_warmup+1),
         #                 # train_loader.num_batches * 32) #options.epoc
-        pretrain_scheduler = cosine_scheduler(optimizer, options.lr, options.post_lr, options.num_warmup_steps / 6, pretrain_loader.num_batches * options.inmodal_warmup)
         total_train_steps = calcualte_num_batches(options, pretrain_loader.num_batches)
         options.train_num_batches = total_train_steps // (options.epochs - options.inmodal_warmup - options.multimodal_warmup)
-        inmodal_scheduler = cosine_scheduler(optimizer, options.lr, options.post_lr, options.num_warmup_steps, total_train_steps)
-        crossmodal_scheduler = cosine_scheduler(optimizer, options.lr, options.post_lr, options.num_warmup_steps, total_train_steps)
+        inmodal_scheduler = cosine_scheduler(optimizer, options.in_lr, options.filter_lr, options.num_warmup_steps, total_train_steps)
+        crossmodal_scheduler = cosine_scheduler(optimizer, options.cross_lr, options.filter_lr, options.num_warmup_steps, total_train_steps)
 
     start_epoch = 0
     dataloader_update_epoch = 0
@@ -176,11 +175,11 @@ def worker(rank, options, logger):
             start = time.time()
             if epoch <= options.inmodal_warmup:
                 logging.info("warm up in-modal training")
-                train(epoch, model, pretrain_loader, optimizer, pretrain_scheduler, scaler, options, memory_bank, inmodal=True)
+                train(epoch, model, pretrain_loader, optimizer, inmodal_scheduler, scaler, options, memory_bank, inmodal=True)
                 # train(epoch, model, train_loader, optimizer, scheduler, scaler, options, memory_bank, inmodal=True)
             elif epoch <= options.multimodal_warmup + options.inmodal_warmup:
                 logging.info("warm up cross-modal training")
-                train(epoch, model, pretrain_loader, optimizer, pretrain_scheduler, scaler, options, memory_bank, inmodal=False)
+                train(epoch, model, pretrain_loader, optimizer, inmodal_scheduler, scaler, options, memory_bank, inmodal=False)
                 # train(epoch, model, train_loader, optimizer, scheduler, scaler, options, caption_memory_bank, inmodal=False)
                 if epoch == options.multimodal_warmup + options.inmodal_warmup:
                     del pretrain_loader
